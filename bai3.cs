881ab348e24d5efcfdf032b28cc03b9335d1ed17
DuVanSang <146832513+DuVanSang@users.noreply.github.com>
2024-05-30 21:58:14 +0700

    Update bai3.cs

--- bai3.cs
+++ bai3.cs
@@ -6,17 +6,10 @@ namespace CharToIntConversion
     {
         static void Main(string[] args)
         {
-            // Ask the user to enter a character
             Console.WriteLine("Please enter a character:");
-
-            // Read the input from the user
             char inputChar = Console.ReadKey().KeyChar;
             Console.WriteLine(); // To move to the next line after the user input
-
-            // Cast the character to an int to get the ASCII code
             int asciiCode = (int)inputChar;
-
-            // Output the result
             Console.WriteLine($"The character you entered is '{inputChar}', its ASCII code is {asciiCode}");
         }
     }
