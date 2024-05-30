2fc7f7381700dfad60a6a7cf926c0b5c9106d9e3
DuVanSang <146832513+DuVanSang@users.noreply.github.com>
2024-05-30 21:59:45 +0700

    Update bai1.cs

--- bai1.cs
+++ bai1.cs
@@ -6,20 +6,10 @@ namespace DataTransformation
     {
         static void Main(string[] args)
         {
-
-            // Ask the user to input an integer
             Console.WriteLine("Please enter an integer:");
-
-            // Read the input from the user
             string input = Console.ReadLine();
-
-            // Convert the input string to an integer
             int number = int.Parse(input);
-
-            // Cast the integer to a double
             double doubleNumber = (double)number;
-
-            // Output the result
             Console.WriteLine($"The number you entered is {number} and as a double is {doubleNumber}");
         }
     }
